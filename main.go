da608614815912cd6d646bf53866141a1e655daf
yujianming <yujianming@macrosan.com>
2025-06-26 16:54:19 +0800

    log add file line

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
+import (
+	"log"
+)
+
 // 实现思路
 // 1. 监听一个tcp端口, 用于处理socks5的连接
 // 2. 启动一个goroutine来处理每个连接
 // 测试方法
 // 1. curl -v --socks5-hostname 172.17.8.219:10801 "https://cn.bing.com/"
 // 其中--socks5-hostname参数指定使用socks5代理
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
 func main() {
 	go startSocks5Listen("10801")
 	go startHttpListen("10802")
